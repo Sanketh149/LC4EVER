74f2c2f28e83a92b428ca14d8222dbff1f7b1ea8
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-03-30 01:21:01 +0530

    Time: 89 ms (76.73%), Space: 12.9 MB (30.61%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -7,33 +7,18 @@ public:
             adj[it[0]][it[1]] = it[2];
             adj[it[1]][it[0]] = it[2];
         }
-        for(int i = 0;i<n;i++)
-        {
-            for(int j = 0;j<n;j++)
-            {
-                if(i == j) adj[i][j] = 0;
-            }
-        }
+        for(int i = 0;i<n;i++)  
+            adj[i][i] = 0;
         vector<vector<int>>dist(adj);
         for(int k = 0;k<n;k++)
-        {
             for(int i = 0;i<n;i++)
-            {
                 for(int j = 0;j<n;j++)
-                {
                     dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
-                }
-            }
-        }
         vector<int>res(n, 0);
         for(int i = 0;i<n;i++)
-        {
             for(int j = 0;j<n;j++)
-            {
                 if(dist[j][i] <= distanceThreshold)
                     res[i]++;
-            }
-        }
         int mini = 1e9;
         int idx = -1;
         for(int i = 0;i<n;i++)
