fee0f7f90db6aedeb0b21cf2d7c4083e4b78160b
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-03-30 17:14:24 +0530

    Time: 97 ms (98.62%), Space: 38.4 MB (87.89%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,72 @@
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+
+class Solution {
+public:
+    int makeConnected(int V, vector<vector<int>>& connections) {
+        int n = connections.size();
+        if(n < V - 1) return -1;
+        DisjointSet dsu(V);
+        int count = 0;
+        for(int i = 0;i<n;i++)
+        {
+            int u = connections[i][0], v = connections[i][1];
+            if(dsu.findUPar(u) != dsu.findUPar(v)) dsu.unionByRank(u, v);
+            else count++;
+        }
+        int comp = 0;
+        for(int i = 0;i<V;i++)
+        {
+            if(i == dsu.findUPar(i)) comp++;
+        }
+        if(count >= comp-1) return comp - 1;
+        return -1;
+    }
+};
