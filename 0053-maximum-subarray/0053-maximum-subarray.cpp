e0c552888422e747ebefbc0cd9ffbfebf183f8a9
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-04-13 01:16:59 +0530

    Time: 120 ms (48.12%), Space: 67.8 MB (17.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int curr = 0, maxi = -1e9;
         for(auto it:nums)
         {
-            curr = max(curr+it, it);
+            curr = max(curr+it, it); //continue the curr subarray or start a new one
             maxi = max(maxi, curr);
         }
         return maxi;
