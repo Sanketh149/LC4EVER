40c67fe1f9961d4929f46298de9a6d4dead3786d
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-04-18 00:26:01 +0530

    Time: 8 ms (95.23%), Space: 13.3 MB (78.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,15 +4,9 @@ public:
         vector<pair<int,int>>v; //row,col
         int m = matrix.size(), n = matrix[0].size();
         for(int i = 0;i<m;i++)
-        {
             for(int j = 0;j<n;j++)
-            {
                 if(matrix[i][j] == 0)
-                {
                     v.push_back({i,j});
-                }
-            }
-        }
         for(auto it:v)
         {
             int r = it.first, c = it.second;
