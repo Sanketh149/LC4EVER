a0bd324473fb5e075205e107c986b137db89ccd5
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-04-20 21:22:41 +0530

    Time: 26 ms (68.85%), Space: 21.1 MB (24.10%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,19 +11,27 @@
  */
 class Solution {
 public:
-    int totalpaths = 0;
-    void rec(TreeNode *root, long target)
-    {
-        if(!root) return;
-        if(target == root->val) totalpaths++;
-        rec(root->left, target-root->val);
-        rec(root->right, target-root->val);
+    int res = 0, sum;
+    unordered_map<long, int> mp;
+    void dfs(TreeNode* root, long partialSum = 0) {
+        // updating partialSum with the current node
+        partialSum += root->val;
+        // checking if we already found an interval summing up to that
+        res += mp[partialSum - sum];
+        // updating seen
+        mp[partialSum]++;
+        // searching potential left and right branches
+        if (root->left) dfs(root->left, partialSum);
+        if (root->right) dfs(root->right, partialSum);
+        // clearing seen of the last value, for backtracking purposes
+        mp[partialSum]--;
     }
     int pathSum(TreeNode* root, int targetSum) {
-        if(!root) return 0;
-        rec(root, targetSum);
-        pathSum(root->left, targetSum);
-        pathSum(root->right, targetSum);
-        return totalpaths;
+        sum = targetSum;
+        // seen set with initial value 0 will help us match from the root
+        mp[0] = 1;
+        if (!root) return 0;
+        dfs(root);
+        return res;
     }
 };
