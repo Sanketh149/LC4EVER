55e57914ea3b131c133c96093c1f2e2eeca6562e
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-05-14 09:51:36 +0530

    Time: 251 ms (53.85%), Space: 69.4 MB (23.08%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -5,37 +5,45 @@ public:
         if(r<0 or c<0 or r>=m or c>=n) return false;
         return true;
     }
-    int dfs(int curr, int col, int row, vector<int> &res, int m, int n, vector<vector<int>> &grid, int &cnt, vector<vector<int>> &dp)
+    void dfs(int curr, int col, int row, vector<int> &res, int m, int n, vector<vector<int>> &grid, int &cnt, vector<vector<int>> &dp)
     {
-        if(!isValid(row, col, m, n)) return 0;
-        if(dp[row][col] != -1) 
-            return dp[row][col];
-        int a = 0,b = 0,c = 0;
+        if(!isValid(row, col, m, n)) return;
+        if(dp[row][col] != 0) 
+            return;
         if(isValid(row-1, col+1, m, n) and grid[row-1][col+1] > grid[row][col])
         {
-            a = dfs(curr, col+1, row-1, res, m, n, grid, cnt, dp);
+            cnt++;
+            dfs(curr, col+1, row-1, res, m, n, grid, cnt, dp);
+            cnt--;
         }
         if(isValid(row, col+1, m,n) and grid[row][col+1] > grid[row][col])
         {
-            b = dfs(curr, col+1, row, res, m, n, grid, cnt, dp);
+            cnt++;
+            dfs(curr, col+1, row, res, m, n, grid, cnt, dp);
+            cnt--;
         }
         if(isValid(row+1, col+1, m, n) and grid[row+1][col+1] > grid[row][col])
         {
-            c = dfs(curr, col+1, row+1, res, m, n, grid, cnt, dp);
+            cnt++;
+            dfs(curr, col+1, row+1, res, m, n, grid, cnt, dp);
+            cnt--;
         }
-        return dp[row][col] = 1+max({a,b,c});
+        res[curr] = max(res[curr], cnt);
+        dp[row][col]  = max(res[curr], dp[row][col]);
     }
     int maxMoves(vector<vector<int>>& grid) {
         int i = 0, j = 0;
         int m = grid.size(), n = grid[0].size();
         vector<int>res(m, 0);
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        int maxi = 0;
+        vector<vector<int>>dp(m, vector<int>(n, 0));
         for(j = 0;j<m;j++)
         {
             int cnt = 0;
-            maxi = max(maxi, dfs(0, 0, j, res, m, n, grid,cnt, dp));
+            dfs(0, 0, j, res, m, n, grid,cnt, dp);
         }
-        return maxi-1;
+        int maxi = 0;
+        for(int i = 0;i<m;i++)
+            maxi = max(maxi, dp[i][0]);
+        return maxi;
     }
 };
