1dc96a4f9158ae861c151e5ec3db5d902a5ec175
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-05-15 22:50:42 +0530

    Time: 698 ms (36.91%), Space: 180.1 MB (54.58%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,28 +11,18 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        int n = 1;
-        int kbeg = 0, kend = 0;
-        ListNode *curr = head;
-        while(curr)
-        {
-            if(n == k) kbeg = curr->val;
-            curr = curr->next;
-            n++;
+        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (--k)
+            ptr1 = ptr1->next;
+        
+        kth = ptr1;
+        ptr1 = ptr1->next;
+        
+        while (ptr1) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
         }
-        curr = head;
-        for(int i = 0;i<n-k-1;i++)
-            curr = curr->next;
-        kend = curr->val;
-        curr = head;
-        int ptr = 1;
-        while(curr)
-        {
-            if(ptr == k) swap(curr->val, kend);
-            else if(ptr == n-k) swap(curr->val, kbeg);
-            curr = curr->next;
-            ptr++;
-        }
+        swap(ptr2->val, kth->val);
         return head;
     }
 };
