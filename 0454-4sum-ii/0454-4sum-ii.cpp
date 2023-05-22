f9d03a79ea4169a7a38b8988888b16ff04a7465c
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-05-22 22:33:50 +0530

    Time: 423 ms (11.96%), Space: 24.8 MB (25.14%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map<int,int>mp;
+        for(auto it1:nums1)
+            for(auto it2:nums2)
+                mp[it1+it2]++;
+        int count = 0;
+        for(auto it3:nums3) //key observation is that one element should be picked from all 4..
+            for(auto it4:nums4)
+                count += mp[-(it3+it4)];
+        return count;
+    }
+};
