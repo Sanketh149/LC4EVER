5f53b89ca4358e4906bf4db9badffc72282f5620
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-05-22 13:52:44 +0530

    Time: 157 ms (7.76%), Space: 49.9 MB (11.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,10 +3,10 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int presum = 0,cnt = 0;
         map<int,int>mp;
-        mp[0] = 1;
         for(auto it:nums)
         {
             presum += it;
+            if(presum == k) cnt++;
             int remaining = presum - k;
             cnt += mp[remaining];
             mp[presum]++;
