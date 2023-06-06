85ba606f5c0f32cc6b50c9191a0fff7620f13e14
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-06 17:00:25 +0530

    Time: 150 ms (35.81%), Space: 23 MB (27.31%) - LeetHub

---
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -0,0 +1,55 @@
+int mod = 1e9+7;
+using ll = long long;
+ll dp[25][235][2];
+class Solution {
+public:
+bool check(ll sum, int mini, int maxi)
+{
+    return sum>=mini && sum<=maxi;
+}
+    
+int G(string number, int mini, int maxi, ll pos = 0, ll sum = 0, ll tight = 1)
+{
+	if(pos == number.size()) 
+	{
+		if(check(sum, mini, maxi)) return 1;
+        return 0;
+	}
+	else if(dp[pos][sum][tight] != -1) return dp[pos][sum][tight];
+	else if(tight == 1)
+	{
+		ll res = 0;
+		for(int i = 0;i<=number[pos]-'0';i++)
+		{
+			if(i == number[pos] - '0')
+				res += G(number, mini, maxi, pos+1, sum+i, 1)%mod;
+			else 
+				res += G(number, mini, maxi, pos+1, sum+i, 0)%mod;
+		}
+		return dp[pos][sum][tight] = res%mod;
+	}
+	else
+	{
+		ll res = 0;
+		for(int i = 0;i<=9;i++)
+		{
+			res += G(number, mini, maxi, pos+1, sum+i, 0)%mod;
+		}
+		return dp[pos][sum][tight] = res%mod;
+	}
+}
+    int count(string num1, string num2, int min_sum, int max_sum) {
+        memset(dp, -1, sizeof(dp));
+        ll ans1 = G(num2, min_sum, max_sum); 
+        memset(dp, -1, sizeof(dp));
+        int n = num1.size();
+        int p = n-1;
+        //Getting num1 - 1 as a string 
+        while(num1[p]=='0') p--; 
+        num1[p++]--;
+        while(p<n) num1[p++] = '9';
+        ll ans2 = G(num1, min_sum, max_sum);
+        int res = (ans1-ans2+mod)%mod;
+        return res;
+    }
+};
