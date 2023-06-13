11a76ea7c396bdb043e7893105c4c1033500b177
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-13 18:56:31 +0530

    Time: 107 ms (69.79%), Space: 32.8 MB (44.75%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        int n = grid.size(), cnt = 0;
-        for(int r = 0;r<n;r++)
-        {
-            for(int c = 0;c<n;c++)
-            {
-                bool flag = true;
-                for(int i = 0;i<n;i++)
-                {
-                    if(grid[r][i] != grid[i][c]) 
-                    {
-                        flag = false;
-                        break;
-                    }
+        int n=grid.size();
+        // storing row into first map
+        map<vector<int>,int>mp;
+            for(int i=0;i<n;i++){
+                mp[grid[i]]++;
+            }
+
+        // storing counlmn into map
+        map<vector<int>,int>mp2;
+            for(int j=0;j<n;j++){
+                vector<int>tmp;
+                for(int i=0;i<n;i++){
+                    tmp.push_back(grid[i][j]);
                 }
-                if(flag) cnt++;
+                mp2[tmp]++;
+            }
+        int ans = 0;
+        //counting number of row and col having equl value
+        for(auto &it:mp){
+            if(mp2.find(it.first)!=mp2.end()){
+                ans+=(it.second)*mp2[it.first];
             }
         }
-        return cnt;
+        return ans;
     }
 };
