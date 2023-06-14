9f4b937bb8fb14e95b03c7d635585b2b555cc6ab
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-14 14:54:12 +0530

    Time: 5 ms (88.03%), Space: 7.1 MB (12.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int left = 0, right = 0,maxi = 0, rem = k;
+        int left = 0, right = 0,maxi = 0, maxFrequent = 0;
         vector<int>res(26, 0);
         while(right<n)
         {
             res[s[right] - 'A']++;
-            int maxFrequent = *max_element(res.begin(), res.end()); 
-            //replacing a in babb is better than replacing b {O(26) only}
+            maxFrequent = max(maxFrequent, res[s[right] - 'A']);
+            //replacing a in babb is better than replacing b {O(1) only}
             while((right - left + 1) - maxFrequent > k)
             {
                 res[s[left] - 'A']--;
