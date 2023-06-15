42aeb375c41afd3c47a5e6b34aa78fb69e24c69c
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-15 19:27:18 +0530

    Time: 43 ms (57.76%), Space: 19 MB (78.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>>nonOverLap;
-        nonOverLap.push_back(intervals[0]);
+        vector<vector<int>>nonOverlaps;
+        nonOverlaps.push_back(intervals[0]);
         for(int i = 1;i<intervals.size();i++)
         {
-            vector<int> &recent = nonOverLap.back();
-            if(intervals[i][0] <= recent[1]) //need to merge
+            auto &recent = nonOverlaps.back();
+            if(recent[1] >= intervals[i][0])
                 recent[1] = max(recent[1], intervals[i][1]);
-            else
-                nonOverLap.emplace_back(intervals[i]);
+            else 
+                nonOverlaps.push_back(intervals[i]);
         }
-        return nonOverLap;
+        return nonOverlaps;
     }
 };
