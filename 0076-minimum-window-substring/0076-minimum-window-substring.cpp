930f4438cf88d76884bac2d5194075d1f2527257
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-16 16:53:19 +0530

    Time: 13 ms (80.73%), Space: 7.9 MB (81.50%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,16 +6,18 @@ public:
         int l = 0, r = 0, m = s.size(), n = t.size(), mini = 1e9, beg = 0;
         while(r<m)
         {
-            if(mp[s[r++]]-- > 0) n--;
+            if(mp[s[r]]-- > 0) n--;
             while(n == 0)
             {
-                if(mini > (r-l))
+                if(mini > (r-l+1))
                 {
-                    mini = r - l;
+                    mini = r-l+1;
                     beg = l;
                 }
-                if(mp[s[l++]]++ == 0) n++;
+                if(mp[s[l]]++ == 0) n++;
+                l++;
             }
+            r++;
         }
         return mini == 1e9?"":s.substr(beg, mini);
     }
