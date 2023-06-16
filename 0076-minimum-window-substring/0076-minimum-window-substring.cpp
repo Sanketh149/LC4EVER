dc436ca44e114fb4e69937aa97215c5d1954f59c
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-16 16:04:41 +0530

    Time: 13 ms (80.73%), Space: 8 MB (58.32%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int>mp(128, 0);
+        for(char it:t) mp[it]++;
+        int l = 0, r = 0, m = s.size(), n = t.size(), mini = 1e9, beg = 0;
+        while(r<m)
+        {
+            if(mp[s[r++]]-- > 0) n--;
+            while(n == 0)
+            {
+                if(mini > (r-l))
+                {
+                    mini = r - l;
+                    beg = l;
+                }
+                if(mp[s[l++]]++ == 0) n++;
+            }
+        }
+        return mini == 1e9?"":s.substr(beg, mini);
+    }
+};
