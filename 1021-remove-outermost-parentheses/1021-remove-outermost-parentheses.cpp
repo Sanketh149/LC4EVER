bcfcceac99a18587e5612776ec200f4372a3afb6
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-18 00:33:06 +0530

    Time: 23 ms (11.29%), Space: 6.8 MB (20.71%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.size();
+        stack<char>st;
+        int i = 1;
+        st.push(s[0]);
+        string ans = "";
+        while(i<n and st.size())
+        {
+            if(st.size() == 1 and i!=n-1 and s[i] == ')')
+            {
+                cout<<i<<endl;
+                st.pop();
+                st.push(s[i+1]);
+                i++;
+            }
+            else
+            {
+                // cout<<i<<endl;
+                if(s[i] == '(') st.push(s[i]);
+                else if(s[i] == ')') st.pop();
+                ans += s[i];
+            }
+            i++;
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
