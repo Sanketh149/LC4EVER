732b287f31ce3129c568f13b7f7c25e26ae73ca4
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-22 19:47:40 +0530

    Time: 45 ms (64.06%), Space: 17.8 MB (42.23%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -25,7 +25,7 @@ public:
     ListNode *getMid(ListNode *head)
     {
         ListNode *slow = head, *fast = head;
-        while(fast->next and fast->next->next)
+        while(fast and fast->next)
         {
             slow = slow->next;
             fast = fast->next->next;
@@ -33,16 +33,25 @@ public:
         return slow;
     }
     void reorderList(ListNode* head) {
+        if(!head or !head->next) return;
         ListNode *mid = getMid(head);
-        ListNode *rev = reverseList(mid->next);
-        mid->next = NULL;
-        while(head and rev)
+        ListNode *rev = reverseList(mid);
+        ListNode *left = head->next;
+        int i = 0;
+        while(left != rev)
         {
-            ListNode *nn = head->next, *rn = rev->next;
-            head->next = rev;
-            head->next->next = nn;
-            head = nn;
-            rev = rn;
+            if(i & 1)
+            {
+                head->next = left;
+                left = left->next;
+            }
+            else
+            {
+                head->next = rev;
+                rev = rev->next;
+            }
+            i++;
+            head = head->next;
         }
     }
 };
