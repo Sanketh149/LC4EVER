b20e54ba4f73d28659e306e67c3f194f934c3c83
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-22 11:46:14 +0530

    Added solution - LeetHub

---
+++ Introduction to Linked List - GFG/introduction-to-linked-list.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        Node *head = new Node(arr[0]);
+        int i = 1;
+        Node *curr = head;
+        while(i<n)
+        {
+            curr->next = new Node(arr[i]);
+            curr = curr->next;
+            i++;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
