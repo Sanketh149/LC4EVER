656924884aa2e81a2758c92c5abae27801b2b097
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-06-24 23:50:31 +0530

    Time: 13 ms (95.77%), Space: 10.5 MB (45.71%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,44 +2,31 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        vector<int>leftMin(n, -1), leftMax(n, -1), rightMin(n, n), rightMax(n, n);
-        stack<int>s;
-        for(int i = 0;i<n;i++)
-        {
-            while(s.size() and nums[s.top()] >= nums[i]) s.pop();
-            if(s.size()) leftMin[i] = s.top();
-            s.push(i);
+        long long answer = 0;
+        stack<int> stk;
+        
+        // Find the sum of all the minimum.
+        for (int right = 0; right <= n; ++right) {
+            while (!stk.empty() && (right == n || nums[stk.top()] >= nums[right])) {
+                int mid = stk.top();
+                stk.pop();
+                int left = stk.empty() ? -1 : stk.top();
+                answer -= (long long)nums[mid] * (right - mid) * (mid - left);
+            }
+            stk.push(right); 
         }
-        while(s.size()) s.pop();
-        for(int i = 0;i<n;i++)
-        {
-            while(s.size() and nums[s.top()] <= nums[i]) s.pop();
-            if(s.size()) leftMax[i] = s.top();
-            s.push(i);
+        
+        // Find the sum of all the maximum.
+        stk.pop();
+        for (int right = 0; right <= n; ++right) {
+            while (!stk.empty() && (right == n || nums[stk.top()] <= nums[right])) {
+                int mid = stk.top();
+                stk.pop();
+                int left = stk.empty() ? -1 : stk.top();
+                answer += (long long)nums[mid] * (right - mid) * (mid - left);
+            }
+            stk.push(right); 
         }
-        while(s.size()) s.pop();
-        for(int i = n-1;i>=0;i--)
-        {
-            while(s.size() and nums[s.top()] > nums[i]) s.pop();
-            if(s.size()) rightMin[i] = s.top();
-            s.push(i);
-        }
-        while(s.size()) s.pop();
-        for(int i = n-1;i>=0;i--)
-        {
-            while(s.size() and nums[s.top()] < nums[i]) s.pop();
-            if(s.size()) rightMax[i] = s.top();
-            s.push(i);
-        }
-        long long res = 0;
-        for(int i = 0;i<n;i++)
-        {
-            long long leftmini = i - leftMin[i];
-            long long rightmini = rightMin[i] - i;
-            long long leftmaxi = i - leftMax[i];
-            long long rightmaxi = rightMax[i] - i;
-            res += 1L*(rightmaxi*leftmaxi - rightmini*leftmini)*nums[i];
-        }
-        return res;
+        return answer;
     }
 };
