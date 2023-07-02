94c82f99800fb6164df453aada5fa40bf8f536a9
Kistappagari Sanketh Kumar <89764074+Sanketh149@users.noreply.github.com>
2023-07-02 14:43:03 +0530

    Time: 122 ms (72.11%), Space: 53.6 MB (71.68%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int,vector<int>, greater<int>>pq;
+        int n = heights.size();
+        for(int i = 0;i<n-1;i++)
+        {
+            int d = heights[i+1] - heights[i];
+            if(d > 0) pq.push(d);
+            if(pq.size() > ladders)
+                bricks -= pq.top(), pq.pop();
+            if(bricks<0)
+                return i;
+        }
+        return n - 1;
+    }
+};
